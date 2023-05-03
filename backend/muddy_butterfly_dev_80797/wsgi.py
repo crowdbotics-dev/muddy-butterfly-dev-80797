30590381ddc0603bb247fec97da9f06ec39db166
Crowdbotics <engineering@crowdbotics.com>
2023-05-03 12:42:02 +0000

    Initial commit.

---
+++ backend/muddy_butterfly_dev_80797/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for muddy_butterfly_dev_80797 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muddy_butterfly_dev_80797.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-butterfly-dev-80797",
+  url: "https://muddy-butterfly-dev-80797.botics.co",
+  api: "https://muddy-butterfly-dev-80797.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
